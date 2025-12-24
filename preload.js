63eb66f922162f5d7fb9b604f96da2f2ff4a77c2
Kishan <90744224+Agent8800@users.noreply.github.com>
2025-12-24 16:04:48 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,10 +2,9 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("api", {
   createComplaint: (payload) => ipcRenderer.invoke("complaint:create", payload),
-  listByMonth: (params) => ipcRenderer.invoke("complaint:listByMonth", params),
-  updateStatus: (params) => ipcRenderer.invoke("complaint:updateStatus", params),
+  listComplaints: (filters) => ipcRenderer.invoke("complaint:list", filters),
   updateComplaint: (payload) => ipcRenderer.invoke("complaint:update", payload),
-
-  exportXlsx: (params) => ipcRenderer.invoke("report:exportXlsx", params),
-  exportPdf: (params) => ipcRenderer.invoke("report:exportPdf", params)
+  monthlyReport: (args) => ipcRenderer.invoke("report:monthly", args),
+  exportExcel: (args) => ipcRenderer.invoke("export:excel", args),
+  exportPdf: (args) => ipcRenderer.invoke("export:pdf", args)
 });
