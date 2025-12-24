f24cde381f9bbb1370cc13c19b301818e6a8d43c
Kishan <90744224+Agent8800@users.noreply.github.com>
2025-12-24 14:07:06 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,5 +4,8 @@ contextBridge.exposeInMainWorld("api", {
   createComplaint: (payload) => ipcRenderer.invoke("complaint:create", payload),
   listByMonth: (params) => ipcRenderer.invoke("complaint:listByMonth", params),
   updateStatus: (params) => ipcRenderer.invoke("complaint:updateStatus", params),
-  exportCsv: (params) => ipcRenderer.invoke("report:exportCsv", params)
+  updateComplaint: (payload) => ipcRenderer.invoke("complaint:update", payload),
+
+  exportXlsx: (params) => ipcRenderer.invoke("report:exportXlsx", params),
+  exportPdf: (params) => ipcRenderer.invoke("report:exportPdf", params)
 });
